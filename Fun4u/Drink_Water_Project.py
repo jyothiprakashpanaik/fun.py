4de7bff74fb29fbeee28378b44ef6ba5535426a3
Janvit __ jp <64550298+jyothiprakashpanaik@users.noreply.github.com>
2020-09-24 22:36:01 +0530

    Update Drink_Water_Project.py

--- Fun4u/Drink_Water_Project.py
+++ Fun4u/Drink_Water_Project.py
@@ -1,6 +1,5 @@
 import time
 from plyer import notification
-````Dont execute
 if __name__=="__main__":
     while True :
         notification.notify(
@@ -8,9 +7,9 @@ if __name__=="__main__":
             message = "Drinking Water Helps Maintain the Balance of Body Fluids. Your body is composed of about 60% water. The functions of these bodily fluids include digestion,absorption, circulation, creation of saliva, transportation of nutrients, and maintenance of body temperature.",
             app_icon = "D:\\USER_D\\Desktop_D\\some file\\py4e\\water.ico",
             message = "Drink Water",
-            # timeout = 10
+            timeout = 10
             )
-        #time.sleep(3)
-        #pythonw .\main.py
+        time.sleep(3)
+        pythonw .\main.py
             
     
