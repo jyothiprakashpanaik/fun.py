625628c6ec8bc0ab2f2e252ac713d574a007d283
Janvit __ jp <64550298+jyothiprakashpanaik@users.noreply.github.com>
2020-09-24 22:35:31 +0530

    Update Drink_Water_Project.py

--- Fun4u/Drink_Water_Project.py
+++ Fun4u/Drink_Water_Project.py
@@ -1,12 +1,12 @@
 import time
 from plyer import notification
 ````Dont execute
-#if __name__=="__main__":
+if __name__=="__main__":
     while True :
         notification.notify(
             title = "Plz.,Drink Water NOW..!",
-            #message = "Drinking Water Helps Maintain the Balance of Body Fluids. Your body is composed of about 60% water. The functions of these bodily fluids include digestion,absorption, circulation, creation of saliva, transportation of nutrients, and maintenance of body temperature.",
-            # app_icon = "D:\\USER_D\\Desktop_D\\some file\\py4e\\water.ico",
+            message = "Drinking Water Helps Maintain the Balance of Body Fluids. Your body is composed of about 60% water. The functions of these bodily fluids include digestion,absorption, circulation, creation of saliva, transportation of nutrients, and maintenance of body temperature.",
+            app_icon = "D:\\USER_D\\Desktop_D\\some file\\py4e\\water.ico",
             message = "Drink Water",
             # timeout = 10
             )
